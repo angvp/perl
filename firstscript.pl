01c7654a051dfe43d8fca614c0fd81d4104d71b5
Angel Velasquez <angvp@archlinux.org>
2011-04-18 15:53:36 -0300

    Adding script with variables, arrays and hashes .. testing the namespaces

---
+++ firstscript.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+# This is an example of how to handle with arrays, hashes and vars, and more
+# important namespaces
+#
+# By Angel 'angvp' Velásquez <angvp@archlinux.org>
+#
+$a = 'Hola mundo'; # this is a string variable 
+@a = (1, 2, 3); # This is an array
+%a = ('a' => 83, 'b' => '86'); # This is a hash
+print "Let's print the var of \$a: ".$a."\n";
+print "Let's print the value of \$a[1]: ".$a[1]."\n";
+print "And then the value of 'a' and 'b' on \%a: ".$a{'a'}." ".$a{'b'}."\n";
